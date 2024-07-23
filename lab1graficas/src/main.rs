d3c81e55c59d33866a9b1430355c8daaee53417d
Giovanni Santos <san22523@uvg.edu.gt>
2024-07-22 23:51:15 -0600

    Create Polygon4

--- lab1graficas/src/main.rs
+++ lab1graficas/src/main.rs
@@ -23,17 +23,52 @@ fn main() {
 
 
     
-    let points = vec![
-        (165, 380)
-        //prueba puntos 
+    // Primer polígono
+    let points1 = vec![
+        Vec3::new(165.0, 380.0, 0.0), Vec3::new(185.0, 360.0, 0.0), Vec3::new(180.0, 330.0, 0.0),
+        Vec3::new(207.0, 345.0, 0.0), Vec3::new(233.0, 330.0, 0.0), Vec3::new(230.0, 360.0, 0.0),
+        Vec3::new(250.0, 380.0, 0.0), Vec3::new(220.0, 385.0, 0.0), Vec3::new(205.0, 410.0, 0.0),
+        Vec3::new(193.0, 383.0, 0.0)
     ];
-    let points_vec3: Vec<Vec3> = points.iter()
-        .map(|&(x, y)| Vec3::new(x as f32, y as f32, 0.0))
-        .collect();
+
+    framebuffer.draw_polygon(&points1);
+
+    // Segundo polígono
+    let points2 = vec![
+        Vec3::new(321.0, 335.0, 0.0), Vec3::new(288.0, 286.0, 0.0), Vec3::new(339.0, 251.0, 0.0),
+        Vec3::new(374.0, 302.0, 0.0)
+    ];
+
+    framebuffer.draw_polygon(&points2);
+
+    //poligono 3
+    let points3 = vec![
+        Vec3::new(377.0, 249.0, 0.0), Vec3::new(411.0, 197.0, 0.0), Vec3::new(436.0, 249.0, 0.0)
+    ];
+
+    framebuffer.draw_polygon(&points3);
 
 
+    //poligono 4 y 5
 
-    framebuffer.draw_polygon(&points_vec3);
+    let points4 = vec![
+        Vec3::new(413.0, 177.0, 0.0), Vec3::new(448.0, 159.0, 0.0), Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0), Vec3::new(535.0, 36.0, 0.0), Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0), Vec3::new(750.0, 145.0, 0.0), Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0), Vec3::new(659.0, 214.0, 0.0), Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0), Vec3::new(580.0, 230.0, 0.0), Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0), Vec3::new(517.0, 144.0, 0.0), Vec3::new(466.0, 180.0, 0.0)
+    ];
+
+    framebuffer.draw_polygon(&points4);
+
+    let points5 = vec![
+        Vec3::new(682.0, 175.0, 0.0), Vec3::new(708.0, 120.0, 0.0), Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0)
+    ];
+
+    framebuffer.draw_polygon(&points5);
+
     
     framebuffer.render_buffer("out.bmp");
         
